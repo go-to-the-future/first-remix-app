f3be1026be0fee7922899eddab90572525b3e564
KiyotoRyuman <appleumasugi.mjd@gmail.com>
2021-11-28 20:50:20 +0900

    fix: errorsの型を変更

--- app/post.ts
+++ app/post.ts
@@ -9,17 +9,11 @@ export type Post = {
   title: string;
 };
 
-type NewPost = {
-  title: string;
-  slug: string;
-  markdown: string;
-};
-
 export type PostMarkdownAttributes = {
   title: string;
 };
 
-let postsPath = path.join(__dirname, "..", "posts");
+let postsPath = path.join(__dirname, "../posts");
 
 function isValidPostAttributes(
   attributes: any
@@ -27,45 +21,41 @@ function isValidPostAttributes(
   return attributes?.title;
 }
 
-export async function getPosts() {
-    let dir = await fs.readdir(postsPath);
-    return Promise.all(
-      dir.map(async filename => {
-        let file = await fs.readFile(
-          path.join(postsPath, filename)
-        );
-        let { attributes } = parseFrontMatter(
-          file.toString()
-        );
-        invariant(
-          isValidPostAttributes(attributes),
-          `${filename} has bad meta data!`
-        );
-        return {
-          slug: filename.replace(/\.md$/, ""),
-          title: attributes.title
-        };
-      })
-    );
-  }
-
-export async function getPost(slug: string) {
-    let filepath = path.join(postsPath, slug + ".md");
-    let file = await fs.readFile(filepath);
-    let { attributes, body } = parseFrontMatter(file.toString());
-    invariant(
-      isValidPostAttributes(attributes),
-      `Post ${filepath} is missing attributes`
-    );
-    let html = marked(body);
-    return { slug, html, title: attributes.title };
-  }
+type NewPost = {
+  title: string;
+  slug: string;
+  markdown: string;
+};
 
 export async function createPost(post: NewPost) {
-    let md = `---\ntitle: ${post.title}\n---\n\n${post.markdown}`;
-    await fs.writeFile(
-      path.join(postsPath, post.slug + ".md"),
-      md
-    );
-    return getPost(post.slug);
-  }
+  let md = `---\ntitle: ${post.title}\n---\n\n${post.markdown}`;
+  await fs.writeFile(path.join(postsPath, post.slug + ".md"), md);
+  return getPost(post.slug);
+}
+
+export async function getPost(slug: string) {
+  let filepath = path.join(postsPath, slug + ".md");
+  let file = await fs.readFile(filepath);
+  let { attributes, body } = parseFrontMatter(file.toString());
+  invariant(
+    isValidPostAttributes(attributes),
+    `Post ${filepath} is missing attributes`
+  );
+  let html = marked(body);
+  return { slug, html, title: attributes.title };
+}
+
+export async function getPosts() {
+  let dir = await fs.readdir(postsPath);
+  return Promise.all(
+    dir.map(async filename => {
+      let file = await fs.readFile(path.join(postsPath, filename));
+      let { attributes } = parseFrontMatter(file.toString());
+      invariant(isValidPostAttributes(attributes));
+      return {
+        slug: filename.replace(/\.md$/, ""),
+        title: attributes.title
+      };
+    })
+  );
+}
--- app/routes/admin/new.tsx
+++ app/routes/admin/new.tsx
@@ -3,12 +3,6 @@ import type { ActionFunction } from "remix";
 import { createPost } from "~/post";
 import invariant from "tiny-invariant";
 
-type ErrorTypes = {
-  title: boolean,
-  slug: boolean,
-  markdown: boolean
-}
-
 export let action: ActionFunction = async ({ request }) => {
   await new Promise(res => setTimeout(res, 1000));
   let formData = await request.formData();
@@ -17,7 +11,7 @@ export let action: ActionFunction = async ({ request }) => {
   let slug = formData.get("slug");
   let markdown = formData.get("markdown");
 
-  let errors: ErrorTypes = {title: false, slug: false, markdown: false};
+  let errors: Record<string, boolean> = {};
   if (!title) errors.title = true;
   if (!slug) errors.slug = true;
   if (!markdown) errors.markdown = true;
@@ -42,21 +36,18 @@ export default function NewPost() {
       <Form method="post">
         <p>
           <label>
-            Post Title:{" "}
-            {errors?.title && <em>Title is required</em>}
+            Post Title: {errors?.title && <em>Title is required</em>}
             <input type="text" name="title" />
           </label>
         </p>
         <p>
           <label>
-            Post Slug:{" "}
-            {errors?.slug && <em>Slug is required</em>}
+            Post Slug: {errors?.slug && <em>Slug is required</em>}
             <input type="text" name="slug" />
           </label>
         </p>
         <p>
-          <label htmlFor="markdown">Markdown:</label>{" "}
-          {errors?.markdown && <em>Markdown is required</em>}
+          <label htmlFor="markdown">Markdown:</label> {errors?.markdown && <em>Markdown is required</em>}
           <br />
           <textarea rows={20} name="markdown" />
         </p>
